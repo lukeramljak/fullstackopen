35414508f303a27605c932f486cf9ec394efab74
Luke Ramljak <71169775+lukeramljak@users.noreply.github.com>
2024-06-19 19:21:44 +0800

    Part 2.7

--- part2/phonebook-frontend/src/App.jsx
+++ part2/phonebook-frontend/src/App.jsx
@@ -4,19 +4,36 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const nameExists = persons.find((person) => person.name === newName);
+    if (nameExists) {
+      alert(`${newName} is already added to phonebook`);
+      return;
+    }
+
+    const personObject = {
+      name: newName,
+    };
+    setPersons(persons.concat(personObject));
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          name: <input />
+          name: <input onChange={(e) => setNewName(e.target.value)} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map((person) => (
+        <p key={person.name}>{person.name}</p>
+      ))}
     </div>
   );
 };
