1374c2f62cb50d760f343b71a0cf5c3196b29371
Luke Ramljak <71169775+lukeramljak@users.noreply.github.com>
2024-06-22 23:04:13 +0800

    4.6

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require("lodash");
+
 const dummy = (blogs) => {
   return 1;
 };
@@ -10,8 +12,18 @@ const favouriteBlog = (blogs) => {
   return blogs.sort((a, b) => a.likes - b.likes)[blogs.length - 1];
 };
 
+const mostBlogs = (blogs) => {
+  const groupedBlogs = _.groupBy(blogs, "author");
+  const authorBlogCounts = _.map(groupedBlogs, (blogs, author) => ({
+    author: author,
+    blogs: blogs.length,
+  }));
+  return _.maxBy(authorBlogCounts, "blogs");
+};
+
 module.exports = {
   dummy,
   totalLikes,
   favouriteBlog,
+  mostBlogs,
 };
