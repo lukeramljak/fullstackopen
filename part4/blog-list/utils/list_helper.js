b8a14d43a94b37dc0073d730009c9009c852d1b5
Luke Ramljak <71169775+lukeramljak@users.noreply.github.com>
2024-06-22 22:03:06 +0800

    4.5

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -6,7 +6,12 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, item) => sum + item.likes, 0);
 };
 
+const favouriteBlog = (blogs) => {
+  return blogs.sort((a, b) => a.likes - b.likes)[blogs.length - 1];
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favouriteBlog,
 };
