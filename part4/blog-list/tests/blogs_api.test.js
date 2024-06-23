7bf860e65e740550620f237f4bf94034b76ac13b
Luke Ramljak <71169775+lukeramljak@users.noreply.github.com>
2024-06-23 12:49:33 +0800

    4.9

--- part4/blog-list/tests/blogs_api.test.js
+++ part4/blog-list/tests/blogs_api.test.js
@@ -1,15 +1,31 @@
-const { test, after } = require("node:test");
+const { test, after, beforeEach } = require("node:test");
+const assert = require("node:assert");
+const Blog = require("../models/blog");
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
+const helper = require("./test_helper");
 
 const api = supertest(app);
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+
+  const blogObjects = helper.initialBlogs.map((blog) => new Blog(blog));
+  const promiseArray = blogObjects.map((blog) => blog.save());
+  await Promise.all(promiseArray);
+});
 
 test.only("blogs are returned as json", async () => {
   await api
     .get("/api/blogs")
     .expect(200)
     .expect("Content-Type", /application\/json/);
+});
+
+test.only("a blog's unique identifier is named 'id'", async () => {
+  const blog = await helper.blogsInDb();
+  assert(Object.keys(blog[0]).includes("id"));
 });
 
 after(async () => {
