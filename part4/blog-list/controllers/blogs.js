198ad401728fdf9d3ce7f1718f9a108017374d8e
Luke Ramljak <71169775+lukeramljak@users.noreply.github.com>
2024-06-30 10:52:57 +0800

    4.17

--- part4/blog-list/controllers/blogs.js
+++ part4/blog-list/controllers/blogs.js
@@ -1,8 +1,12 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
+const User = require("../models/user");
 
 blogsRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate("user", {
+    username: 1,
+    name: 1,
+  });
   response.json(blogs);
 });
 
@@ -12,12 +16,27 @@ blogsRouter.get("/:id", async (request, response) => {
 });
 
 blogsRouter.post("/", async (request, response) => {
-  if (!request.body.title || !request.body.url) {
+  const { title, author, url, likes } = request.body;
+
+  if (!title || !url) {
     return response.status(400).end();
   }
-  const blog = new Blog(request.body);
-  const result = await blog.save();
-  response.status(201).json(result);
+
+  const users = await User.find({});
+  const user = users[0];
+
+  const blog = new Blog({
+    title: title,
+    author: author,
+    url: url,
+    likes: likes,
+    user: user.id,
+  });
+
+  const savedBlog = await blog.save();
+  user.blogs = user.blogs.concat(savedBlog._id);
+  await user.save();
+  response.status(201).json(savedBlog);
 });
 
 blogsRouter.delete("/:id", async (request, response) => {
