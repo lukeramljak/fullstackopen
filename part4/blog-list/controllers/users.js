aad8aa9a27f92591e7fa21de87f7797952ea802d
Luke Ramljak <71169775+lukeramljak@users.noreply.github.com>
2024-06-30 10:07:20 +0800

    Add username/password length validation

--- part4/blog-list/controllers/users.js
+++ part4/blog-list/controllers/users.js
@@ -10,6 +10,18 @@ usersRouter.get("/", async (request, response) => {
 usersRouter.post("/", async (request, response) => {
   const { username, name, password } = request.body;
 
+  if (username.length < 3) {
+    response.status(400).json({
+      error: "username must be 3 or more characters",
+    });
+  }
+
+  if (password.length < 3) {
+    response.status(400).json({
+      error: "password must be 3 or more characters",
+    });
+  }
+
   const saltRounds = 10;
   const passwordHash = await bcrypt.hash(password, saltRounds);
 
